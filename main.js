afbc88f9ccbb4a5b5221242a64ddc78a5e2e995a
Mike Tsao <miket@google.com>
2016-11-09 12:39:51 -0800

    0.0.0.2

---
+++ main.js
@@ -0,0 +1,35 @@
+var elements = document.getElementsByClassName("titletext");
+
+function endsWithPunctuation(s) {
+  if (s.endsWith(".")) {
+    return true;
+  }
+  if (s.endsWith("!")) {
+    return true;
+  }
+  if (s.endsWith("?")) {
+    return true;
+  }
+}
+
+function randomHilariousCatchPhrase() {
+  var CATCH_PHRASES = [
+    "Believe me.",
+    "Bigly.",
+    "I kid you not.",
+    "That is what people are telling me.",
+    "There's something going on."
+    "Trust me.",
+    "You mark my words.",
+  ];
+  return " " + CATCH_PHRASES[Math.floor(Math.random() * CATCH_PHRASES.length)];
+}
+
+for (var i in elements) {
+  var e = elements[i];
+  if (!endsWithPunctuation(e.innerText)) {
+    e.innerText += ".";
+  }
+
+  e.innerText += randomHilariousCatchPhrase();
+}
--- thxbo.js
+++
@@ -1,6 +0,0 @@
-var elements = document.getElementsByClassName("titletext");
-
-for (var i in elements) {
-  var e = elements[i];
-  e.innerText += ". Thanks, Obama!";
-}
